be05f1f9ef4de7fede98548c40c25e1dea07bc42
ziyan-xiaoyu <82202541+ziyan-xiaoyu@users.noreply.github.com>
2024-10-15 23:11:37 +0800

    Update gemini_2_shot_random.py

--- Code/close_models/gemini_2_shot_random.py
+++ Code/close_models/gemini_2_shot_random.py
@@ -5,8 +5,9 @@ from io import BytesIO
 import requests
 import random
 
-genai.configure(api_key="your key", transport="rest")
+# AIzaSyBUyK-PvQk04QJnU815oR1yOc019mGK0GI # 1.5-1
 
+genai.configure(api_key="your key", transport="rest")
 
 generation_config = {"temperature": 0, "top_p": 1, "top_k": 1, "max_output_tokens": 480}
 safety_settings = [
@@ -16,27 +17,27 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}
 ]
 
-FILE_PATH = './datasets/test_en_select_500_sort2.jsonl'
+FILE_PATH = 'test_en_select_500_sort_cp.jsonl'
 IMAGE_DIR = 'http://images.cocodataset.org/test2017'
-RESULT_FILE_PATH = './model_results/gemini_2_shot_random_new.jsonl'
+RESULT_FILE_PATH = 'results/gemini_2_shot_random_1.jsonl'
 
 
 example_list_rule1 = [
-    'Input: Image: "The picture depicts a clock with the time 12:27. From the perspective of the photographer, the clock is facing you and the 12 scale is directly above the 6 scale.", Question: "For the clock in the picture, which side of the 1 scale does the hour hand point to?", Options: "left of; right of". \nOutput: left of.',
-    'Input: Image: "The picture depicts a dining table with white plate, glass, etc. From the perspective of the photographer, the glass cup is further away from you than the white plate.", Question: "Where is the white plate located relative to the glass?", Options: "in front of, behind, left of, right of". \nOutput: in front of.',
-    'Input: Image: "The picture depicts a red warning sign with two lines of English words "STOP WAR" written on it. From the perspective of the photographer, the letter O is above the letter A." Question: "For the letters on the red warning sign, where is the letter W located on the letter T?", Options: "on/above; below; left of; right of". \nOutput: below.'
+    {"id": 1, "image": "000000358641.jpg", "text": "Question: For the clock in the picture, which side of the 1 scale does the hour hand point to?, Options: left of; right of. \nOutput: right of."},
+    {"id": 2, "image": "000000209618.jpg", "text": "Question: Where is the white plate located relative to the glass?, Options: in front of; behind; left of; right of. \nOutput: in front of."},
+    {"id": 3, "image": "000000010682.jpg", "text": "Question: For the letters on the warning sign, where is the letter W located relative to the letter O?, Options: on/above; below; left of; right of. \nOutput: below."}
 ]
 
 example_list_rule2 = [
-    'Input: Image: "The picture depicts a man playing baseball. From the perspective of the photographer, the man is facing you, with the shadow to his left.", Question: "If you are the man in the picture, where is your shadow located relative to you?", Options: "in front of; behind; left of; right of". \nOutput: right of.',
-    'Input: Image: "The picture depicts a woman playing tennis. From the perspective of the photographer, the tennis ball happened to fly over the head of woman", Question: "If you are the woman in the picture, where is the tennis ball located relative to you?", Options: "on/above; below; in front of; behind; left of; right of". \nOutput: on/above.',
-    'Input: Image: "The picture depicts a child sitting on the bed. From the perspective of the photographer, the child is facing you and the window is located on the wall behind the bed.", Question: "If you are the child in the picture, where is the window located relative to you?", Options: "in front of; behind; left of; right of". \nOutput: behind.'
+    {"id": 1, "image": "000000057664.jpg", "text": "Question: If you are the person skiing in the picture, where is your shadow located relative to you?, Options: in front of; behind; left of; right of. \nOutput: right of."},
+    {"id": 2, "image": "000000073924.jpg", "text": "Question: If you were a player playing on the court, where would the tennis ball be located relative to you?, Options: on/above; below; in front of; behind; left of; right of. \nOutput: on/above."},
+    {"id": 3, "image": "000000022707.jpg", "text": "Question: If you were the little girl in the picture, where would the window be located relative to you?, Options: in front of; behind; left of; right of. \nOutput: behind."}
 ]
 
 example_list_rule3 = [
-    'Input: Image: "This picture depicts a bus and a stroller. From the perspective of the photographer, the bus is heading towards you, and the stroller is located on the right side of the bus.", Question: "If you are the driver of the bus in the picture, from your perspective, where is the stroller located on the bus?", Options: "in front of; behind; left of; right of". \nOutput: left of.',
-    'Input: Image: "The picture depicts a dask table with laptop, scissors, etc. From the perspective of the photographer, the laptop is facing to the left front, and there is a stack of books on the right front of the laptop with a pair of scissors on them.", Question: "If you are sitting in front of the computer in the picture, where is the scissors located relative to the laptop from your perspective?", Options: "on/above; below; in front of; behind; left of; right of". \nOutput: right of.',
-    'Input: Image: "The picture depicts a white car and a motorcycle. From the perspective of the photographer, there is a motorcycle in the middle, with a white car behind it and the rear of car facing you.", Question: "If you are the driver of the white car in the picture, from your perspective, where is the motorcycle located relative to the car?", Options: "in front of; behind; left of; right of". \nOutput: behind.'
+    {"id": 1, "image": "000000139664.jpg", "text": "Question: If you are the driver of the bus in the picture, from your perspective, where is the stroller located relative to the bus?, Options: in front of; behind; left of; right of. \nOutput: left of."},
+    {"id": 2, "image": "000000221101.jpg", "text": "If you are sitting in front of the computer in the picture, where is the scissors located relative to the laptop from your perspective?, Options: on/above; below; in front of; behind; left of; right of. \nOutput: right of."},
+    {"id": 3, "image": "000000164692.jpg", "text": "Question: If you are the driver of the white car in the picture, from your perspective, where is the motorcycle located relative to the car?, Options: in front of; behind; left of; right of. \nOutput: behind."}
 ]
 
 
@@ -59,13 +60,14 @@ E52 = random.choice(example_list_rule3)
 E61 = random.choice(example_list_rule3)
 E62 = pop_exist(E61, example_list_rule3)
 
+
 example_list = [
-    'Example 1 ' + E11 + '\nExample 2 ' + E12,
-    'Example 1 ' + E21 + '\nExample 2 ' + E22,
-    'Example 1 ' + E31 + '\nExample 2 ' + E32,
-    'Example 1 ' + E41 + '\nExample 2 ' + E42,
-    'Example 1 ' + E51 + '\nExample 2 ' + E52,
-    'Example 1 ' + E61 + '\nExample 2 ' + E62
+    [E11, E12],
+    [E21, E22],
+    [E31, E32],
+    [E41, E42],
+    [E51, E52],
+    [E61, E62]
 ]
 
 
@@ -80,7 +82,7 @@ def fetch_image_content(image_url):
 import PIL.Image as Image
 
 # few-shot & zero-shot:
-model = genai.GenerativeModel('gemini-pro-vision', generation_config=generation_config, safety_settings=safety_settings)
+model = genai.GenerativeModel('gemini-1.5-flash', generation_config=generation_config, safety_settings=safety_settings)
 
 # text-only:
 # model = genai.GenerativeModel('gemini-pro', generation_config=generation_config, safety_settings=safety_settings)
@@ -95,35 +97,58 @@ with open(FILE_PATH, 'r', encoding="utf-8") as f, open(RESULT_FILE_PATH, 'a', en
         data = json.loads(line)
         id = data['id']
 
-        # 1 - shot:
-        question = f'You are currently a senior expert in spatial relation reasoning. ' \
-                   f'\nGiven an Image, a Question and Options, your task is to answer the correct spatial relation. Note that you only need to choose one option from the all options without explaining any reason.' \
-                   f'\nGiven the following 1 examples to learn the spatial relation reasoning task:' \
-                   f'\n{example_list[count % 6]} ' \
-                   f'Input: Image:<image>, Question: {data["question"]}, Options: {"; ".join(data["options"])}. \nOutput: '
+        example_2 = example_list[count % 6]
 
-        image_name = data['image']
-        image_filepath = f'{IMAGE_DIR}/{image_name}'
+        # 2 - shot
+        prompt = ['', '', '', '']
+        prompt[0] = f'You are currently a senior expert in spatial relation reasoning. ' \
+                    f'\nGiven an Image, a Question and Options, your task is to answer the correct spatial relation. Note that you only need to choose one option from the all options without explaining any reason.' \
+                    f'\nGiven the following 2 examples to learn the spatial relation reasoning task:' \
+                    f'\nExample1: Input: Image: '
+        prompt[1] = f'\n{example_2[0]["text"]} ' \
+                    f'\nExample2: Input: Image: '
+        prompt[2] = f'\n{example_2[1]["text"]} ' \
+                    f'\nInput: Image: '
 
-        image_content = fetch_image_content(image_filepath)
+        # prompt = f'Given the following 3 examples to learn the spatial relation reasoning task:' \
+        #          f'\n{example_list[count % 10][0]["text"]} ' \
+        #          f'\nYou are currently a senior expert in spatial relation reasoning. ' \
+        #          f'\nGiven an Image, a Question and Options, your task is to answer the correct spatial relation. Note that you only need to choose one option from the all options without explaining any reason.' \
+        #          f'\nInput: Image: '
+        prompt[3] = f'\nQuestion: {data["question"]}, Options: {"; ".join(data["options"])}. \nOutput: '
+
+        image_list = []
+        for item in example_2:
+            image_id = item['image']
+            image_url = f'{IMAGE_DIR}/{image_id}'
+            image_content = fetch_image_content(image_url)
+            if image_content is not None:
+                image = Image.open(image_content)
+                image_list.append(image)
+
+        image_id = data['image']
+        image_url = f'{IMAGE_DIR}/{image_id}'
+        image_content = fetch_image_content(image_url)
+
         if image_content is not None:
             image = Image.open(image_content)
+            image_list.append(image)
             try:
                 response = model.generate_content(
-                    [question, image]    # few-shot & zero-shot
+                    [prompt[0], image_list[0], prompt[1], image_list[1], prompt[2], image_list[2], prompt[3]]    # few-shot & zero-shot
                     # [question]         # text-only
                 )
             except Exception as e:
                 print(e)
                 try:
                     response = model.generate_content(
-                        [question, image]    # few-shot & zero-shot
+                        [prompt[0], image_list[0], prompt[1], image_list[1], prompt[2], image_list[2], prompt[3]]    # few-shot & zero-shot
                         # [question]         # text-only
                     )
                 except Exception as e:
                     try:
                         response = model.generate_content(
-                            [question, image]    # few-shot & zero-shot
+                            [prompt[0], image_list[0], prompt[1], image_list[1], prompt[2], image_list[2], prompt[3]]    # few-shot & zero-shot
                             # [question]         # text-only
                         )
                     except Exception as e:
@@ -134,12 +159,14 @@ with open(FILE_PATH, 'r', encoding="utf-8") as f, open(RESULT_FILE_PATH, 'a', en
             try:
                 output = response.text.strip().rstrip('.').lower()
             except Exception as e:
+                print(e)
+                output = '--'
                 result_json = {"id": id, "result": 0, "output": output.lower(), "answer": data['answer'], "rule": data['rule'], "example": count % 6 + 4}
                 fout.write(json.dumps(result_json, ensure_ascii=False) + '\n')
                 count += 1
                 continue
             count += 1
-            if output in data['answer']:
+            if output in data['answer'] or data['answer'] in output:
                 result_json = {"id": id, "result": 1, "output": output.lower(), "answer": data['answer'], "rule": data['rule'], "example": count % 6 + 4}
                 fout.write(json.dumps(result_json, ensure_ascii=False) + '\n')
                 right_count += 1
